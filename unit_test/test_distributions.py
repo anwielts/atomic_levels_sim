fc801a7cb65e21b89e8cf54cb1d0db1dec711ead
ACW <andreaswieltsch@gmail.com>
2019-08-01 22:49:17 +0200

    Updated test cases after renaming functions

--- unit_test/test_distributions.py
+++ unit_test/test_distributions.py
@@ -2,17 +2,30 @@
 import sys
 import unittest
 import env
-from distributions import maxwell_boltzmann_distribution
+from distributions import maxwell_boltzmann_distribution_v_2, maxwell_boltzmann_distribution_v_3
 
 
 class DistributionsTest(unittest.TestCase):
 
-    def testMaxwellBoltzmann(self):
-        self.assertLessEqual(maxwell_boltzmann_distribution(0, 6.94*1.66E-27, 650), 1, msg=None)
-        self.assertLessEqual(maxwell_boltzmann_distribution(5000, 6.94*1.66E-27, 650), 1, msg=None)
-        self.assertGreaterEqual(maxwell_boltzmann_distribution(0, 6.94*1.66E-27, 650), 0, msg=None)
-        self.assertGreaterEqual(maxwell_boltzmann_distribution(5000, 6.94*1.66E-27, 650), 0, msg=None)
+    def testMaxwellBoltzmannV2(self):
+        self.assertLessEqual(maxwell_boltzmann_distribution_v_2(0, 6.94*1.66E-27, 650), 1, msg=None)
+        self.assertLessEqual(maxwell_boltzmann_distribution_v_2(5000, 6.94*1.66E-27, 650), 1, msg=None)
+        self.assertGreaterEqual(maxwell_boltzmann_distribution_v_2(0, 6.94*1.66E-27, 650), 0, msg=None)
+        self.assertGreaterEqual(maxwell_boltzmann_distribution_v_2(5000, 6.94*1.66E-27, 650), 0, msg=None)
+        self.assertLessEqual(maxwell_boltzmann_distribution_v_2(0, 1.0*1E27, 1000), 1, msg=None)
+        self.assertLessEqual(maxwell_boltzmann_distribution_v_2(10000, 1.0*1E-27, 650), 1, msg=None)
+        self.assertGreaterEqual(maxwell_boltzmann_distribution_v_2(1, 1, 1), 0, msg=None)
+        self.assertGreaterEqual(maxwell_boltzmann_distribution_v_2(5000, 6.94*1.66E-27, 1E-15), 0, msg=None)
 	
+    def testMaxwellBoltzmannV3(self):
+        self.assertLessEqual(maxwell_boltzmann_distribution_v_3(0, 6.94*1.66E-27, 650), 1, msg=None)
+        self.assertLessEqual(maxwell_boltzmann_distribution_v_3(5000, 6.94*1.66E-27, 650), 1, msg=None)
+        self.assertGreaterEqual(maxwell_boltzmann_distribution_v_3(0, 6.94*1.66E-27, 650), 0, msg=None)
+        self.assertGreaterEqual(maxwell_boltzmann_distribution_v_3(5000, 6.94*1.66E-27, 650), 0, msg=None)
+        self.assertLessEqual(maxwell_boltzmann_distribution_v_3(0, 1.0*1E27, 1000), 1, msg=None)
+        self.assertLessEqual(maxwell_boltzmann_distribution_v_3(10000, 1.0*1E-27, 650), 1, msg=None)
+        self.assertGreaterEqual(maxwell_boltzmann_distribution_v_3(1, 1, 1), 0, msg=None)
+        self.assertGreaterEqual(maxwell_boltzmann_distribution_v_3(5000, 6.94*1.66E-27, 1E-15), 0, msg=None)
 	
 if __name__ == "__main__":
     unittest.main()
--- unit_test/test_light_atom_interaction.py
+++ unit_test/test_light_atom_interaction.py
@@ -2,8 +2,7 @@
 import sys
 import unittest
 import env
-from light_atom_interaction import doppler_shift, lorentzian_probability
-
+from light_atom_interaction import doppler_shift, lorentzian_probability, laser_intensity_gauss, scatter_rate
 
 
 class Light_Atom_Interaction_Test(unittest.TestCase):
@@ -15,10 +14,27 @@ class Light_Atom_Interaction_Test(unittest.TestCase):
         self.assertEqual(doppler_shift(0, 1000, 0, 5E14, 0,0,1, 670.977E-9), 5E14, msg=None)
 		
     def testLorentzianProbability(self):
-        self.assertLessEqual(lorentzian_probability(1, 3, 2, 1, 1, 1), 1/66, msg=None)
-        self.assertLessEqual(lorentzian_probability(1, 3, 2, 1, -1, 1), 1/64, msg=None)
-        self.assertGreaterEqual(lorentzian_probability(1, 3, 2, 2, 3, 4), 4/71, msg=None)
-        self.assertGreaterEqual(lorentzian_probability(1, 3, -2, 1, 1, -2), 2, msg=None)
+        self.assertLessEqual(lorentzian_probability(1, 3, 2, 1, 1, 1), 0.01, msg=None)
+        self.assertLessEqual(lorentzian_probability(1, 3, 2, 1, 2, 1), 6E-4, msg=None)
+        self.assertGreaterEqual(lorentzian_probability(1, 3, 2, 2, 3, 4), 0.005, msg=None)
+        self.assertGreaterEqual(lorentzian_probability(1, 3, -2, 1, 1, 2), 0.001, msg=None)
+        self.assertLessEqual(lorentzian_probability(1, 3, 2E6, 1, 1, 1), 1.0E-14, msg=None)
+        self.assertLessEqual(lorentzian_probability(1, 3, 2E6, 1, 2, 1), 1.0E-14, msg=None)
+        self.assertGreaterEqual(lorentzian_probability(1, 3, 2E6, 2, 3, 4), 0.0, msg=None)
+        self.assertGreaterEqual(lorentzian_probability(1, 3, -2E6, 1, 1, 2), 0.0, msg=None)
+
+		
+    def testLaserIntensityGauss(self):
+        self.assertEqual(laser_intensity_gauss(0.0002, 1.0, 1.0), 1.7815252551728327, msg=None)
+        self.assertEqual(laser_intensity_gauss(0.0001, 0.5, 0.1), 0.25642497617439414, msg=None)
+        self.assertEqual(laser_intensity_gauss(1.0E-5, 0.1, 10.0), 21.087579151860186, msg=None)
+        self.assertEqual(laser_intensity_gauss(1.0E-7, 0.01, 0.01), 0.010215416304041422, msg=None)
+		
+    # def testScatterRate(self):
+    #     self.assertEqual(scatter_rate(, , , , ), , msg=None)
+    #     self.assertEqual(scatter_rate(, , , , ), , msg=None)
+    #     self.assertEqual(scatter_rate(, , , , ), , msg=None)
+    #     self.assertEqual(scatter_rate(, , , , ), , msg=None)
 	
 if __name__ == "__main__":
     unittest.main()
